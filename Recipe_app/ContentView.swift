59a68c5fe5e0c6a10a559f52c8d6b916eda802c8
Dev Patel <dpatel222430@gmail.com>
2024-05-29 18:35:12 +0530

    Initial Commit

---
+++ Recipe_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Recipe_app
+//
+//  Created by Dev Patel on 29/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
