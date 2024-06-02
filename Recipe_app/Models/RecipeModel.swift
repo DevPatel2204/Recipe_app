be915cce50f89265df1f91e1fbbae94976d0ed81
Dev Patel <dpatel222430@gmail.com>
2024-06-03 01:06:03 +0530

    data added

---
+++ Recipe_app/Models/RecipeModel.swift
@@ -0,0 +1,8 @@
+//
+//  RecipeModel.swift
+//  Recipe_app
+//
+//  Created by Dev Patel on 30/05/24.
+//
+
+import Foundation
---
+++ Recipe_app/View/Components/Recipecard.swift
@@ -0,0 +1,18 @@
+//
+//  Recipecard.swift
+//  Recipe_app
+//
+//  Created by Dev Patel on 03/06/24.
+//
+
+import SwiftUI
+
+struct Recipecard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Recipecard()
+}
