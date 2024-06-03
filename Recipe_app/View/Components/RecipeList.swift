bd0aa59b1f84577585172b037b146257f8e55fbb
Dev Patel <dpatel222430@gmail.com>
2024-06-03 18:23:14 +0530

    Home View Updated by each recipe

---
+++ Recipe_app/View/Components/RecipeList.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeList.swift
+//  Recipe_app
+//
+//  Created by Dev Patel on 03/06/24.
+//
+
+import SwiftUI
+
+struct RecipeList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeList()
+}
