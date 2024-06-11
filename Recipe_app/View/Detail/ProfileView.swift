d2a5f7caa3cb0bf47849ea0d0df5dbe02899914d
Dev Patel <dpatel222430@gmail.com>
2024-06-11 20:25:40 +0530

    fix:Settings and Profile

---
+++ Recipe_app/View/Detail/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Recipe_app
+//
+//  Created by Dev Patel on 11/06/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
